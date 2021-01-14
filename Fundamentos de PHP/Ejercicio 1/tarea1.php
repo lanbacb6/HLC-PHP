0582b1f913265ffcb7ae167c347bc53d3a72f081
lanbacb6 <alum.jmariamorenov.2020@iesalixar.org>
2021-01-14 15:55:05 +0100

    Ejercicio 1 PHP Completado

---
+++ Fundamentos de PHP/Ejercicio 1/tarea1.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "Con función echo podemos imprimir directamente varias cadenas a la vez. <br>";
+
+    print_r("Con la función print solo podremos imprimir una cadena cada vez que sea llamada.");
+
+
+
+
