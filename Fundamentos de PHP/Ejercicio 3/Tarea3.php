70fe101b4e9e1fa1d8bfa527ccd24539554aede7
lanbacb6 <alum.jmariamorenov.2020@iesalixar.org>
2021-01-20 16:37:54 +0100

    Ejercicio 3 Terminado

---
+++ Fundamentos de PHP/Ejercicio 3/Tarea3.php
@@ -0,0 +1,10 @@
+<?php
+   // Definicion de constante.
+   define("constanteNumero", 3);
+   define("constanteCadena", "Hola");
+
+   echo "La constanteNumero contiene este valor: ".constanteNumero. "<br>"; 
+   print_r("La constanteCadena contiene este valor: ".constanteCadena);
+?>
+
+
