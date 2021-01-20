8abd97ded7f3ec4decc13c968a956343b1dcc4a8
lanbacb6 <alum.jmariamorenov.2020@iesalixar.org>
2021-01-20 17:07:52 +0100

    Ejercicios Terminado

---
+++ Fundamentos de PHP/Ejercicio 4/Tarea4.php
@@ -0,0 +1,13 @@
+<?php
+
+    $variable = "2";
+
+    
+    echo "La variable contiene este valor: $variable<br>";
+
+    $variable = $variable = "3";
+
+    echo "Nuevo valor de variable es: $variable";
+
+
+
---
+++ Fundamentos de PHP/Ejercicio 6/Tarea6.php
@@ -0,0 +1,18 @@
+<?php
+
+    $color="verde";
+
+    $$color="es horrible";
+
+    print $$color;
+
+    print ${$color};
+
+    print $verde;
+
+
+?>
+
+¿Qué ocurre cuando la variable previa cambia de valor?. 
+
+Cabiara la variable al ultimo valor introducido.
